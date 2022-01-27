68a1d7c95bc84dc22f515cc939ca7bab430c4620
Arseny Egorov <arsegorov@gmail.com>
2022-01-27 05:06:40 -0800

    Excluded main from coverage

--- class_variable/post_definition.py
+++ class_variable/post_definition.py
@@ -6,9 +6,14 @@ class PostdefinedVariable:
 
 PostdefinedVariable.one_list = [PostdefinedVariable.add_one(n) for n in range(1)]
 
-if __name__ == "__main__":
+
+def main() -> None:  # pragma: no cover
     from pprint import pprint
 
     print(f"dir(Class1):")
     pprint(dir(PostdefinedVariable), compact=True, indent=4)
     print(f"Class1.one_list: {PostdefinedVariable.one_list}")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
