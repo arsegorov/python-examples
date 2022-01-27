f453dda9b9023fb7e392408853112e042e3df74b
Arseny Egorov <arsegorov@gmail.com>
2022-01-27 03:11:21 -0800

    Renamed class

--- class_variable/post_definition.py
+++ class_variable/post_definition.py
@@ -1,14 +1,14 @@
-class Class1:
+class PostdefinedVariable:
     @staticmethod
     def add_one(n: int) -> int:
         return n + 1
 
 
-Class1.one_list = [Class1.add_one(n) for n in range(1)]
+PostdefinedVariable.one_list = [PostdefinedVariable.add_one(n) for n in range(1)]
 
 if __name__ == "__main__":
     from pprint import pprint
 
     print(f"dir(Class1):")
-    pprint(dir(Class1), compact=True, indent=4)
-    print(f"Class1.one_list: {Class1.one_list}")
+    pprint(dir(PostdefinedVariable), compact=True, indent=4)
+    print(f"Class1.one_list: {PostdefinedVariable.one_list}")
