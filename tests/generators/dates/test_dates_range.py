d5dbc69ff702362ad8a1a7b29ec33626249d0a6c
Arseny Egorov <arsegorov@gmail.com>
2022-01-29 04:08:22 -0800

    Simple test for dates_range

---
+++ tests/generators/dates/test_dates_range.py
@@ -0,0 +1,12 @@
+from datetime import date
+import logging
+from generators.dates import dates_range
+
+
+def test_dates_range(caplog):
+    caplog.clear()
+    g = dates_range("2021-01-01", 5)
+    l = [d for d in g]
+    assert len([r for r in caplog.record_tuples if r[1] == logging.INFO]) == 1
+    assert len([r for r in caplog.record_tuples if r[1] == logging.DEBUG]) == 4 * 5 + 1
+    assert l == [date(2021, 1, i) for i in range(1, 6)]
