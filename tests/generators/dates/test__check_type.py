02d958bd617fc560387a5cec796db706f878d0dc
Arseny Egorov <arsegorov@gmail.com>
2022-01-29 20:02:08 -0800

    Test for _check_type

---
+++ tests/generators/dates/test__check_type.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+import pytest
+from generators.dates import _check_type
+
+
+def test__check_type():
+    with pytest.raises(
+        TypeError, match=r"5 is expected to be 'float'\. Instead got 'int'"
+    ):
+        _check_type(5, "5", float)
+
+    with pytest.raises(
+        TypeError, match=r"5 is expected to be one of \('float',\)\. Instead got 'int'"
+    ):
+        _check_type(5, "5", (float,))
+
+    with pytest.raises(
+        TypeError,
+        match=r"5 is expected to be one of \('float', 'str'\)\. Instead got 'int'",
+    ):
+        _check_type(5, "5", (float, str))
+
+    assert _check_type(5, "5", int) is None
+    assert _check_type(5, "5", (int,)) is None
+    assert _check_type(5, "5", (int, float)) is None
